c578d9783a09a44ac4dddf501817a24de5c01bae
PeterSuh-Q3 <dante9000@gmail.com>
2023-03-19 12:16:17 +0900

    kernel 4.4.302+

--- src/compile.sh
+++ src/compile.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-TOOLKIT_VER="7.1"
+TOOLKIT_VER="7.2"
 
-if [ -f ../../arpl/PLATFORMS ]; then
-  cp ../../arpl/PLATFORMS PLATFORMS
-else
-  curl -sLO "https://github.com/fbelavenuto/arpl/raw/main/PLATFORMS"
-fi
+#if [ -f ../../arpl/PLATFORMS ]; then
+#  cp ../../arpl/PLATFORMS PLATFORMS
+#else
+#  curl -sLO "https://github.com/fbelavenuto/arpl/raw/main/PLATFORMS"
+#fi
 
 echo -e "Compiling modules..."
 while read PLATFORM KVER; do
