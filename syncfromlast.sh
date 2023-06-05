bc3e84840a862a062c01c093bb53b6d7a52b55ca
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2023-06-05 22:43:03 +0900

    Update syncfromlast.sh

--- syncfromlast.sh
+++ syncfromlast.sh
@@ -1,8 +1,8 @@
 for D in `ls -d *-*/`; do
-            E=$(echo ${D} | sed 's#/##')
-            echo "download ${E}.tgz"
-            curl -kL https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/${E}.tgz -o /tmp/${E}.tgz
-            echo "extract ${E} peter's module pack"
-            tar -xaf /tmp/${E}.tgz -C ./${E}/
+    E=$(echo ${D} | sed 's#/##')
+    echo "download ${E}.tgz"
+    curl -kL https://github.com/PeterSuh-Q3/arpl-modules/releases/latest/download/${E}.tgz -o /tmp/${E}.tgz
+    echo "extract ${E} peter's module pack"
+    tar -xaf /tmp/${E}.tgz -C ./${E}/
 done
 
